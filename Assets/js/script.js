e1e5a00a487486c0ed9dc6db5bffb04a808eeb1e
Mark Edwards <109709559+mark-227-g@users.noreply.github.com>
2023-03-05 12:32:34 -0500

    Added functions to dynamically create hour bands for calendar

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -21,3 +21,85 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+/************************************** 
+global variables
+**************************************/
+var workdayStart=9;
+var workdayEnd=18;
+
+/************************************** 
+functions
+**************************************/
+
+/************************************** 
+Get the current time and compare the 
+current hour to the parameter.
+return the corresponding class name
+hour format: military time
+**************************************/
+function getHourClass(hour)
+{
+  var d=new Date();
+  var currentHour=d.getHours();
+  var strClass="";
+
+  if (hour == currentHour)
+  { 
+    strClass="present";
+  }
+  else if (hour < currentHour)
+  {
+    strClass="past";
+  }
+  else
+  {
+    strClass="future";
+  }
+
+  return(strClass);
+};
+
+/************************************** 
+convert military time hour to 
+standard time hour
+add am or pm to time
+**************************************/
+function ampm(hour)
+{
+  if(hour>12)
+  {
+    return((hour-12))+ " pm";
+  }
+  else
+  {
+    return (hour+" am");
+  }
+}
+
+/************************************** 
+create hour bands for the calendar
+**************************************/
+function createDayCalendar()
+{
+  var calendarDayEl = document.querySelector("#calendarDay");
+  var timeBlockEl;
+
+  for (var dayHour = workdayStart;dayHour<=workdayEnd;dayHour++)
+  {
+    timeBlockEl = document.createElement("div");
+    timeBlockEl.innerHTML='<div id="hour-' + dayHour + '" class="row time-block ' + getHourClass(dayHour)+ 
+    '"><div class="col-2 col-md-1 hour text-center py-3">' + ampm(dayHour) + '</div>' +
+    '<textarea class="col-8 col-md-10 description" rows="3"> </textarea>' +
+    '<button class="btn saveBtn col-2 col-md-1" aria-label="save">' +
+     '<i class="fas fa-save" aria-hidden="true"></i>' +
+     '</button>' +
+     '</div>';
+     calendarDayEl.appendChild(timeBlockEl);
+  };
+};
+
+/************************************** 
+create calendar on load
+**************************************/
+$(document).ready(createDayCalendar);
