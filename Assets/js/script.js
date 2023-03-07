53638a14e02a6b0cf926d147513a0a150dc5e878
Mark Edwards <109709559+mark-227-g@users.noreply.github.com>
2023-03-07 12:02:52 -0500

    Cleaned up multiple parts of the application while working with tutor

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,32 +1,19 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
 /************************************** 
 global variables
 **************************************/
 var workdayStart=9;
-var workdayEnd=18;
+var workdayEnd=17;
+
+var saveBtnEl = document.querySelector('#saveBtn');
+
+class workdayEvent {
+  constructor(hour, text) {
+    this.hour = hour;
+    this.text = text;
+  }
+};
+
+myWorkday=[];
 
 /************************************** 
 functions
@@ -35,6 +22,7 @@ function main()
 {
   setHeadingDate();
   createDayCalendar();
+  loadmyWorkday();
 
 }
 /************************************** 
@@ -76,21 +64,25 @@ function ampm(hour)
   {
     return((hour-12))+ " pm";
   }
+  else if(hour == 12)
+  {
+    return (hour+" pm");
+  }
   else
   {
     return (hour+" am");
   }
 }
 
+/************************************** 
+
+**************************************/
 function setHeadingDate()
 {
   var d=new Date();
-//var advancedFormat = require('dayjs/plugin/advancedFormat');
-//  dayjs.extend(advancedFormat);
- // var advancedFormat = require('dayjs/plugin/advancedFormat');
-dayjs.extend(window.dayjs_plugin_advancedFormat);
-  document.querySelector("#currentDay").textContent = dayjs(d).format('dddd, MMMM, D');
+  document.querySelector("#currentDay").textContent = moment(d).format('dddd, MMMM, Do');
 }
+
 /************************************** 
 create hour bands for the calendar
 **************************************/
@@ -98,19 +90,65 @@ function createDayCalendar()
 {
   var calendarDayEl = document.querySelector("#calendarDay");
   var timeBlockEl;
+  var btnEl;
 
   for (var dayHour = workdayStart;dayHour<=workdayEnd;dayHour++)
   {
     timeBlockEl = document.createElement("div");
     timeBlockEl.innerHTML='<div id="hour-' + dayHour + '" class="row time-block ' + getHourClass(dayHour)+ 
     '"><div class="col-2 col-md-1 hour text-center py-3">' + ampm(dayHour) + '</div>' +
-    '<textarea class="col-8 col-md-10 description" rows="3"> </textarea>' +
-    '<button class="btn saveBtn col-2 col-md-1" aria-label="save">' +
+    '<textarea id="txt-'+dayHour+'" class="col-8 col-md-10 description" rows="3"> </textarea>' +
+    '<button id="btn-'+dayHour+'" class="btn saveBtn col-2 col-md-1" aria-label="save" value="'+dayHour+'">' +
      '<i class="fas fa-save" aria-hidden="true"></i>' +
      '</button>' +
      '</div>';
      calendarDayEl.appendChild(timeBlockEl);
+     btnEl=document.getElementById("btn-"+dayHour);
+     btnEl.addEventListener("click",saveEvent);
   };
+};
+
+/************************************** 
+Get my workday events from local storage
+fill the workday data
+**************************************/
+function loadmyWorkday()
+{
+  myWorkday=JSON.parse(localStorage.getItem("myWorkday"));
+  $.each( myWorkday, function( key, value ) 
+  {
+    document.getElementById("txt-"+value.hour).value=value.text;
+  });
+}
+
+/************************************** 
+Triggered by button press
+1) get hour from value of button pressed
+2) look for a workdayEvent object
+3) if found update text
+4) if not found create new workdayEvent
+5) save to local storage
+**************************************/
+var saveEvent = function(event)
+{
+
+var hour=event.currentTarget.value;
+var text=document.getElementById("txt-"+hour).value;
+
+
+i = myWorkday.findIndex(workdayEvent => workdayEvent.hour==hour);
+
+if(i>=0)
+{
+ myWorkday[i].text=text;
+}
+else
+{
+  var newWorkdayEvent= new workdayEvent(hour,text);
+  myWorkday.push(newWorkdayEvent);
+}
+
+localStorage.setItem("myWorkday", JSON.stringify(myWorkday));
 };
 
 /************************************** 
